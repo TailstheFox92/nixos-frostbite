5253f2be5fc757aa9cf08202bce7668f0401ce71
Gabriel Fernandez <gfernandez@mailfence.com>
2026-03-10 12:26:25 -0400

    Alacritty theming

--- home/modules/desktop-sway.nix
+++ home/modules/desktop-sway.nix
@@ -286,7 +286,53 @@ in
   programs.alacritty = {
     enable = true;
     settings = {
-      font.size = 12.0;
+      font = {
+        normal = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Italic";
+        };
+        size = 12.0;
+      };
+
+      colors = {
+        primary = {
+          background = "#282828";
+          foreground = "#ebdbb2";
+        };
+        cursor = {
+          text = "#282828";
+          cursor = "#fe8019";
+        };
+        normal = {
+          black = "#282828";
+          red = "#cc241d";
+          green = "#98971a";
+          yellow = "#d79921";
+          blue = "#458588";
+          magenta = "#b16286";
+          cyan = "#689d6a";
+          white = "#a89984";
+        };
+        bright = {
+          black = "#928374";
+          red = "#fb4934";
+          green = "#b8bb26";
+          yellow = "#fabd2f";
+          blue = "#83a598";
+          magenta = "#d3869b";
+          cyan = "#8ec07c";
+          white = "#ebdbb2";
+        };
+      };
+
       window.opacity = 0.9;
     };
   };
